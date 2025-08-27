ed10a319e948a4855d121f7fcafa18a52f92718a
Mark <85582966+MarkPalkimas@users.noreply.github.com>
2025-08-27 16:21:33 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/AI-Education/', // must match GitHub repo name
+  base: '/AI-Education/', // must match repo name
+  build: {
+    outDir: 'docs', // 👈 GitHub Pages will serve this folder
+  },
 })
