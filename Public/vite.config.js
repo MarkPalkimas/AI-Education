013ee32da77293727d25afdcaa06f5b7b51e3c44
Mark <85582966+MarkPalkimas@users.noreply.github.com>
2025-08-27 13:41:17 -0400

    Update vite.config.js

--- Public/vite.config.js
+++ Public/vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/AI-Education/", // <-- set this to your repo name
+  base: "/AI-Education/",   // <-- MUST match repo name exactly (case-sensitive!)
 });
